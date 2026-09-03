09b98347a812ac2afa3041b55f98951191ccddec
claudia <claudiabeltran.apr@gmail.com>
2026-09-03 12:51:55 +0200

    feat: differtent post it colors (added at column)

--- postit.js
+++ postit.js
@@ -2,10 +2,14 @@ const { ipcRenderer } = require('electron');
 const postit = document.getElementById('postit');
 const closeBtn = document.getElementById('close-btn');
 const dots = document.querySelectorAll('.dot');
+const colorDots = document.getElementById('color-dots');
 const addBtn = document.getElementById('add-btn');
+const upArrowBtn = document.getElementById('up-arrow');
+const downArrowBtn = document.getElementById('down-arrow')
 
 const COLS = 5;
 const ROWS = 5;
+let currentRow = 0; // show dots
 
 //
 function setTile(el, col, row) {
@@ -14,17 +18,33 @@ function setTile(el, col, row) {
     el.style.backgroundPosition = `${posX}% ${posY}%`;
 }
 
-dots.forEach(dot => {
-    const col = parseInt(dot.dataset.col);
-    const row = parseInt(dot.dataset.row);
-    setTile(dot, col, row);
+function updateDots() {
+    dots.forEach(dot => {
+        const col = parseInt(dot.dataset.col);
+        setTile(dot, col, currentRow);
+    });
+}
 
+dots.forEach(dot => {
     dot.addEventListener('click', () => {
-        setTile(postit, col, row);
+        const col = parseInt(dot.dataset.col);
+        setTile(postit, col, currentRow);
         postit.dataset.col = col;
-        postit.dataset.row = row;
+        postit.dataset.row = currentRow;
     });
 });
+
+upArrowBtn.addEventListener('click', () => {
+    currentRow = (currentRow - 1 + ROWS) % ROWS;
+    updateDots();
+})
+
+downArrowBtn.addEventListener('click', () => {
+    currentRow = (currentRow + 1 + ROWS) % ROWS;
+    updateDots();
+})
+
+updateDots();
 
 // Initial color: row 0, col 0
 setTile(postit, 0, 0); 
